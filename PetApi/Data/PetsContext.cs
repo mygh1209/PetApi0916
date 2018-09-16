d5e8c85fe10514186794ee9e0a3f396c6672d262
mygh1209 <1127425163@qq.com>
2018-09-16 21:45:59 +0800

    Add project files.

---
+++ PetApi/Data/PetsContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace PetApi.Models
+{
+    public class PetsContext : DbContext
+    {
+        public PetsContext (DbContextOptions<PetsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PetApi.Models.Pet> Pet { get; set; }
+    }
+}
---
+++ PetApi/Models/Pet.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PetApi.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+
+        public string Owner { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Age { get; set; }
+
+    }
+}
